16afcf75a574b7364b8b8db96ae1968025b96d49
Mohammad Imran Shaik <imran@example.com>
2026-09-15 14:59:45 -0500

    Intitial Bookstore Application

---
+++ app.py
@@ -0,0 +1,6 @@
+print("================================")
+print("        WELCOME TO BOOKSTORE    ")
+print("================================")
+
+print("BookStore application is running!")
+print("Welcome to our online bookstore.")
